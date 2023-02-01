f70ddd6dbea42ab5a0c50f3443c216740bc2343e
Anderson Mendes <mendes.ams@gmail.com>
2023-01-31 23:49:55 -0300

    Criação do projeto de estoque de carros

---
+++ src/main/java/com/andersonmendes/estoquecarros/EstoquecarrosApiApplication.java
@@ -0,0 +1,13 @@
+package com.andersonmendes.estoquecarros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoquecarrosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstoquecarrosApiApplication.class, args);
+	}
+
+}
