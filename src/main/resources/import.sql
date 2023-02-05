3fea5991b1bdd2b2d1b771a7fecfd687e90e9f1f
Anderson Mendes <mendes.ams@gmail.com>
2023-02-04 22:42:20 -0300

    Inclusão da dependencia FLAWAY no projeto, com criação do BD, tabela LOJA e tabela CARRO através da primeira versão da MIGRATION.

---
+++ src/main/resources/import.sql
@@ -0,0 +1,23 @@
+INSERT INTO loja (nome, endereco, telefone) 
+VALUES 
+('Show Multimarcas', 'SMPW', 'Quadra 25 lote 24'),
+('Imports Showroom ', 'SMPW', 'Quadra 25 lote 24'),
+('Nacional Car', 'SMPW', 'Quadra 25 lote 24'),
+('', 'SMPW', 'Quadra 25 lote 24');
+
+INSERT INTO carro (nome, marca, modelo, ano, cor, placa, loja_id) VALUES ('Civic', 'Honda', '2015', 'Prata', 'SDF5894', 1);
+INSERT INTO carro (nome, marca, modelo, ano, cor, placa, loja_id) VALUES ('Astra', 'Chevrolet', '2005', 'Grafite', 'JJJ7812', 1);
+INSERT INTO carro (nome, marca, modelo, ano, cor, placa, loja_id) VALUES ('Uno', 'Fiat', '2011', 'Amarelo', 'GSM1245', 1);
+INSERT INTO carro (nome, marca, modelo, ano, cor, placa, loja_id) VALUES ('Wolkswagen', 'Voyage', '2020', 'Praeto', 'KTR6873', 2);
+INSERT INTO carro (nome, marca, modelo, ano, cor, placa, loja_id) VALUES ('408', 'Peugeot', '2015', 'Branco', '', 2);
+INSERT INTO carro (nome, marca, modelo, ano, cor, placa, loja_id) VALUES ('Civic', 'Honda', '2015', 'Prata', 'SDF5894', 2);
+INSERT INTO carro (nome, marca, modelo, ano, cor, placa, loja_id) VALUES ('Civic', 'Honda', '2015', 'Prata', 'SDF5894', 3);
+INSERT INTO carro (nome, marca, modelo, ano, cor, placa, loja_id) VALUES ('Civic', 'Honda', '2015', 'Prata', 'SDF5894', 3);
+INSERT INTO carro (nome, marca, modelo, ano, cor, placa, loja_id) VALUES ('Civic', 'Honda', '2015', 'Prata', 'SDF5894', 3);
+INSERT INTO carro (nome, marca, modelo, ano, cor, placa, loja_id) VALUES ('Civic', 'Honda', '2015', 'Prata', 'SDF5894', 4);
+
+
+
+
+
+
