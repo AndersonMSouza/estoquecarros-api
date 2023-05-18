3cabc150bc1f75efcf8dbfe16e75dfbd31f3b82e
Anderson Mendes <mendes.ams@gmail.com>
2023-05-17 23:31:28 -0300

    Refatorando método adicionar.

--- src/main/java/com/andersonmendes/estoquecarros/api/controller/LojaController.java
+++ src/main/java/com/andersonmendes/estoquecarros/api/controller/LojaController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.andersonmendes.estoquecarros.domain.exceptions.EntidadeEmUsoException;
@@ -43,13 +44,9 @@ public class LojaController {
 	}
 	
 	@PostMapping
-	public ResponseEntity<?> adicionar(@RequestBody Loja loja) {
-		try {
-			loja = cadastroLojaService.salvar(loja);
-			return ResponseEntity.status(HttpStatus.CREATED).body(loja);
-		} catch (EntidadeNaoEncontradaException e) {
-			return ResponseEntity.badRequest().body(e.getMessage());
-		}
+	@ResponseStatus(HttpStatus.CREATED)
+	public Loja adicionar(@RequestBody Loja loja) {
+		return cadastroLojaService.salvar(loja);
 	}
 	
 	@PutMapping("/{lojaId}")
