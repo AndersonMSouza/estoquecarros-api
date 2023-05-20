b5e46dad839ab42d531d93f52c287f09d279a3db
Anderson Mendes <mendes.ams@gmail.com>
2023-05-19 22:13:37 -0300

    Refatorando método remover.

--- src/main/java/com/andersonmendes/estoquecarros/api/controller/LojaController.java
+++ src/main/java/com/andersonmendes/estoquecarros/api/controller/LojaController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,8 +15,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.andersonmendes.estoquecarros.domain.exceptions.EntidadeEmUsoException;
-import com.andersonmendes.estoquecarros.domain.exceptions.EntidadeNaoEncontradaException;
 import com.andersonmendes.estoquecarros.domain.model.Loja;
 import com.andersonmendes.estoquecarros.domain.repository.LojaRepository;
 import com.andersonmendes.estoquecarros.domain.service.CadastroLojaService;
@@ -58,16 +55,8 @@ public class LojaController {
 	}
 	
 	@DeleteMapping("/{lojaId}")
-	public ResponseEntity<?> remover(@PathVariable Long lojaId) {
-		try {
-			cadastroLojaService.excluir(lojaId);
-			return ResponseEntity.noContent().build();
-			
-		} catch (EntidadeNaoEncontradaException e) {
-			return ResponseEntity.notFound().build();
-		
-		} catch (EntidadeEmUsoException e){
-			return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());		
-		}
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable Long lojaId) {
+		cadastroLojaService.excluir(lojaId);
 	}
 }
