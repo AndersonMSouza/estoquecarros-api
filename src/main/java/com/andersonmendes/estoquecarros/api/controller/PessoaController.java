a11d9fbabe19f51cb1f9860bf64f0c077b56ede5
Anderson Mendes <mendes.ams@gmail.com>
2023-05-21 22:23:00 -0300

    Criando método listar.

--- src/main/java/com/andersonmendes/estoquecarros/api/controller/PessoaController.java
+++ src/main/java/com/andersonmendes/estoquecarros/api/controller/PessoaController.java
@@ -1,5 +1,24 @@
 package com.andersonmendes.estoquecarros.api.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.andersonmendes.estoquecarros.domain.model.Pessoa;
+import com.andersonmendes.estoquecarros.domain.repository.PessoaRepository;
+
+@RestController
+@RequestMapping("/pessoas")
 public class PessoaController {
 
+	@Autowired
+	private PessoaRepository pessoaRepository;
+	
+	@GetMapping
+	public List<Pessoa> listar() {
+		return pessoaRepository.findAll();
+	}
 }
