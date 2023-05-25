6682c62c001860101b180d61ce7f0667cad8bb22
Anderson Mendes <mendes.ams@gmail.com>
2023-05-24 21:36:28 -0300

    Criando método atualizar.

--- src/main/java/com/andersonmendes/estoquecarros/api/controller/PessoaController.java
+++ src/main/java/com/andersonmendes/estoquecarros/api/controller/PessoaController.java
@@ -3,12 +3,14 @@ package com.andersonmendes.estoquecarros.api.controller;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,6 +48,24 @@ public class PessoaController {
 			pessoa = pessoaRepository.save(null);
 			return ResponseEntity.status(HttpStatus.CREATED).body(pessoa);
 		
+		} catch (EntidadeNaoEncontradaException e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+	}
+	
+	@PutMapping("/{pessoaId}")
+	public ResponseEntity<?> atualizar(@PathVariable Long pessoaId, @RequestBody Pessoa pessoa) {
+		try {
+			Optional<Pessoa> pessoaAtual = pessoaRepository.findById(pessoaId);
+		
+			if (pessoaAtual.isPresent()) {
+				BeanUtils.copyProperties(pessoa, pessoaAtual.get(), "id");
+				Pessoa pessoaSalva = pessoaRepository.save(pessoaAtual.get());
+				return ResponseEntity.ok(pessoaSalva);
+			}
+			
+			return ResponseEntity.notFound().build();
+			
 		} catch (EntidadeNaoEncontradaException e) {
 			return ResponseEntity.badRequest().body(e.getMessage());
 		}
