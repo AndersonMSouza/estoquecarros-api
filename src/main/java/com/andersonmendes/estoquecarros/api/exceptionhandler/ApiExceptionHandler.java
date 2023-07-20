286d1ca2313fe23ee729cebc49fdf075097cc15d
Anderson Mendes <mendes.ams@gmail.com>
2023-07-19 23:26:20 -0300

    Refatorando ExceptionsHandlers.

--- src/main/java/com/andersonmendes/estoquecarros/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/andersonmendes/estoquecarros/api/exceptionhandler/ApiExceptionHandler.java
@@ -2,37 +2,35 @@ package com.andersonmendes.estoquecarros.api.exceptionhandler;
 
 import java.time.LocalDateTime;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.andersonmendes.estoquecarros.domain.exceptions.EntidadeEmUsoException;
 import com.andersonmendes.estoquecarros.domain.exceptions.EntidadeNaoEncontradaException;
 import com.andersonmendes.estoquecarros.domain.exceptions.NegocioException;
 
 @ControllerAdvice
-public class ApiExceptionHandler {
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 	
 	@ExceptionHandler(EntidadeNaoEncontradaException.class)
 	public ResponseEntity<?> tratarEntidadeNaoEncontradaException(
-			EntidadeNaoEncontradaException e) {
-		Problema problema = Problema.builder()
-				.dataHora(LocalDateTime.now())
-				.mensagem(e.getMessage()).build();
+			EntidadeNaoEncontradaException ex, WebRequest request) {
 		
-		return ResponseEntity.status(HttpStatus.NOT_FOUND)
-			.body(problema);
+		return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), 
+				HttpStatus.NOT_FOUND, request);
 	}
 
 	@ExceptionHandler(NegocioException.class)
-	public ResponseEntity<?> tratarNegocioException(NegocioException e) {
-		Problema problema = Problema.builder()
-				.dataHora(LocalDateTime.now())
-				.mensagem(e.getMessage()).build();
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-				.body(problema);
+	public ResponseEntity<?> tratarNegocioException(NegocioException ex, WebRequest request) {
+		return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), 
+				HttpStatus.BAD_REQUEST, request);
 	}
 
 	@ExceptionHandler(HttpMediaTypeNotSupportedException.class)
@@ -45,12 +43,29 @@ public class ApiExceptionHandler {
 	}
 	
 	@ExceptionHandler(EntidadeEmUsoException.class)
-	public ResponseEntity<?> tratarEntidadeEmUsoException(EntidadeEmUsoException e) {
-		Problema problema = Problema.builder()
-				.dataHora(LocalDateTime.now())
-				.mensagem(e.getMessage()).build();
-		return ResponseEntity.status(HttpStatus.CONFLICT)
-				.body(problema);
-	}
+	public ResponseEntity<?> tratarEntidadeEmUsoException(
+			EntidadeEmUsoException ex, WebRequest request) {
+		
+		return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), 
+				HttpStatus.CONFLICT, request);
+	}	
 	
+	@Override
+	protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers,
+			HttpStatusCode status, WebRequest request) {
+		
+		if (body == null) {
+			body = Problema.builder()
+					.dataHora(LocalDateTime.now())
+					.mensagem(status.toString())
+					.build();
+		} else if (body instanceof String) {
+			body = Problema.builder()
+					.dataHora(LocalDateTime.now())
+					.mensagem((String) body)
+					.build();
+		}
+		
+		return super.handleExceptionInternal(ex, body, headers, status, request);
+	}
 }
